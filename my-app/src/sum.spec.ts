f4b19ceef90dd532ac38d549e9891fb3051ae7d1
kchang <2335501+kchang4@users.noreply.github.com>
2023-06-20 08:54:38 +0000

    revert back to next.js

--- my-app/src/sum.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect } from "@jest/globals";
-import { sum } from "./sum";
-
-describe("sum module", () => {
-  test("1 + 2 = 3", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
